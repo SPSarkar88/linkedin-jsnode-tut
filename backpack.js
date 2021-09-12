95c0ec30c35aeff14f29be00f4266ab23dec69d3
SPSarkar88 <subhraprakash.bw@gmail.com>
2021-09-12 11:04:59 +0530

    feat: Added new class backpack and its constructor

---
+++ backpack.js
@@ -0,0 +1,15 @@
+class Backpack {
+  constructor(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen){
+    this.name = name;
+    this.volume = volume; 
+    this.color = color;
+    this.pocketNum = pocketNum; 
+    thsi.strapLength = {
+      left = strapLengthL,
+      right = strapLengthR
+    },
+    thsi.lidOpen = false
+  }
+
+}
+
