59976168f48313cde7f26a830c7e9575a9c54492
SPSarkar88 <subhraprakash.bw@gmail.com>
2021-09-12 11:12:31 +0530

    feat: Compoleted backpack feature

--- backpack.js
+++ backpack.js
@@ -1,15 +1,33 @@
 class Backpack {
+  /**
+   * Constructor
+   */
   constructor(name, volume, color, pocketNum, strapLengthL, strapLengthR, lidOpen){
     this.name = name;
     this.volume = volume; 
     this.color = color;
     this.pocketNum = pocketNum; 
-    thsi.strapLength = {
+    this.strapLength = {
       left = strapLengthL,
       right = strapLengthR
     },
-    thsi.lidOpen = false
+    this.lidOpen = false
   }
 
+  /**
+   * Lid Status
+   */
+  toggleLid(lidStatus){
+    this.lidOpen = lidStatus;
+  }
+
+  /**
+   * Change Strap Length
+   */
+  changeStrapLength(lengthL, lengthR){
+    this.strapLength.left = lengthL;
+    this.strapLength.right = lengthR;
+  }
 }
 
+export default Backpack;
