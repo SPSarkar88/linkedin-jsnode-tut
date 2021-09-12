a7a235c637ae1ea924e42a9fc00987c9478e736e
SPSarkar88 <subhraprakash.bw@gmail.com>
2021-09-12 11:17:09 +0530

    feat: Integrated backpack into index js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+import BackpPack from './backpack.js';
+
+const backpack = new BackpPack('NIKE', 50, 'Black', 4, 4, 8, true);
+console.log(backpack);
